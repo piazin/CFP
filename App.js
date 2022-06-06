3dabc00c6266ec3679fc8323d7f6ff015ac3c7b0
piazin <ls4803326@gmail.com>
2022-06-06 20:49:08 -0300

    disable yellowBox

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import {StatusBar} from "react-native";
 import Routes from "./src/routes";
 import AuthProvider from "./src/contexts/auth";
 
+console.ignoredYellowBox=true;
+
 export default function App(){
   return(
     <NavigationContainer>
